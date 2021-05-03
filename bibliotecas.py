dd96ea39904ce8f9486a5cba309a2e112c02aa4d
joão Pedro Souza <72163013+souzaexe@users.noreply.github.com>
2021-05-03 11:06:10 -0300

    funções embaralhar e colorir baralho

--- bibliotecas.py
+++ bibliotecas.py
@@ -1,3 +1,5 @@
+import random
+
 def cria_baralho():
     faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     naipes = ['♠','♥','♦','♣']
@@ -13,3 +15,34 @@ def cria_baralho():
             j+=1
         i+=1
     return baralho
+
+baralho = cria_baralho()
+embaralhado=[]
+while len(embaralhado) < len(baralho):
+    carta = random.choice(baralho)
+    if carta not in embaralhado:
+        embaralhado.append(carta)
+
+
+#Funcao para colorir o baralho:
+def colore_baralho(baralho):
+    i=0
+    tamanho=len(baralho)
+    baralho_color=[]
+    while i < tamanho:
+        item = baralho[i]
+        teste = extrai_naipe(item)
+        valor=0
+        if teste == '♠':
+            valor = '\033[34m' + item + '\033[0m'
+        elif teste == '♥':
+            valor = '\033[31m' + item + '\033[0m'
+        elif teste == '♦':
+            valor = '\033[32m' + item + '\033[0m'
+        elif teste == '♣':
+            valor = '\033[33m' + item + '\033[0m'
+
+        baralho_color.append(valor)
+        i+=1
+        
+    return baralho_color
