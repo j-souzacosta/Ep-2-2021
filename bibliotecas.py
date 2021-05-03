6f664ea7d1c6ffcd6767ce8ba3eab425140f2819
jmanoelinsper <joaompf@al.insper.edu.br>
2021-05-03 10:37:43 -0300

    funcao cria baralho

---
+++ bibliotecas.py
@@ -0,0 +1,15 @@
+def cria_baralho():
+    faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    naipes = ['♠','♥','♦','♣']
+    baralho = []
+    i=0
+    while i < len(faces):
+        item = faces[i]
+        j=0
+        while j < len(naipes):
+            naipe = naipes[j]
+            carta = item + naipe
+            baralho.append(carta)
+            j+=1
+        i+=1
+    return baralho
