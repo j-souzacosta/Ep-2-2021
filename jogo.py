3d15fe77a6f246fa69ad14e1ef9e5cef5974e1de
jmanoelinsper <joaompf@al.insper.edu.br>
2021-05-09 22:49:22 -0300

    loop para o jogo continuar rodando

--- jogo.py
+++ jogo.py
@@ -1,5 +1,10 @@
+vitoria = True
+loop = True 
 #Loop para o jogo continuar rodando ate nao houver mais possibilidades de movimento
 while loop:
 
     baralho = cria_baralho()
     embaralhado = embaralha_baralho(baralho)
+
+    #Loop para o jogo continuar rodando ate o tamanho do baralho ser igual a 1
+    while len(embaralhado) > 1:
