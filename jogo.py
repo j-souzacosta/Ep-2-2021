c994df38babd99bf2332ec8dc0a7ecf55ad3b1e2
joão Pedro Souza <72163013+souzaexe@users.noreply.github.com>
2021-05-09 22:57:36 -0300

    mostrando as cartas do baralho

--- jogo.py
+++ jogo.py
@@ -8,3 +8,9 @@ while loop:
 
     #Loop para o jogo continuar rodando ate o tamanho do baralho ser igual a 1
     while len(embaralhado) > 1:
+        for i,carta in enumerate(embaralhado):
+            print(f'{i + 1}. {colore_carta(carta)}')
+
+        movimentos_possiveis = verifica_movimentos_possiveis(embaralhado)
+        print('Quantidade de movimentos possiveis: {0}'.format(len(movimentos_possiveis)))
+        
