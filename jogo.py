89708c99d6f62d527a90d9418bf69b88fae52d11
jmanoelinsper <joaompf@al.insper.edu.br>
2021-05-10 19:39:26 -0300

    verificando se ha movimentos possiveis

--- jogo.py
+++ jogo.py
@@ -17,11 +17,22 @@ while loop:
         print('Quantidade de movimentos possiveis: {0}'.format(len(movimentos_possiveis)))
         
         if len(movimentos_possiveis) == 0 and len(embaralhado) > 1:
-        vitoria = False
-        break   
+            vitoria = False
+            break   
+        
         carta= int(input(f'Escolha uma carta (Digite um numero de 1 a {len(embaralhado)}): ')) - 1        
+        
         while carta >= len(embaralhado) or carta < 1:
             print('Numero invalido')
             carta= int(input(f'Escolha outra carta (Digite um numero de 1 a {len(embaralhado)}): ')) - 1
 
+        lista_mov = []
+        lista_mov = lista_movimentos_possiveis(embaralhado,carta)
+        destino = 0 
+        
+        while len(lista_mov) == 0:
+            print('Essa carta nao pode ser movida')
+            carta= int(input(f'Escolha outra carta (Digite um numero de 1 a {len(embaralhado)}): ')) - 1
+            lista_mov = lista_movimentos_possiveis(embaralhado,carta)
+        
         
